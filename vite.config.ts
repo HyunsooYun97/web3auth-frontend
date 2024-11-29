84806b297652e5da73b2af797ea6ded18d0bb168
HyunsooYun97 <hyensooyoon@primeblock.co>
2024-11-29 20:45:19 +0900

    fix: 빌드시 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    nodePolyfills({
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   resolve: {
     alias: {
+      // buffer polyfill
+      stream: 'vite-plugin-node-polyfills/polyfills/stream',
+      buffer: 'vite-plugin-node-polyfills/polyfills/buffer',
+      util: 'vite-plugin-node-polyfills/polyfills/util',
+      process: 'vite-plugin-node-polyfills/polyfills/process',
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
