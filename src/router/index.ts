d623acd2585a0455d7dd3221da35ddecb901e1ab
HyunsooYun97 <hyensooyoon@primeblock.co>
2024-12-02 10:32:54 +0900

    test: 로그인 테스트3

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import redirect from './redirect';
 // this generates a separate chunk (About.[hash].js) for this route
 // which is lazy-loaded when the route is visited.
 const SignInView = () => import('../views/SignInView.vue');
+const WalletView = () => import('../views/WalletView.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
       path: '/sign-in',
       name: 'sign-in',
       component: SignInView,
+    },
+    {
+      path: '/wallet',
+      name: 'wallet',
+      component: WalletView,
     },
     ...redirect,
   ],
