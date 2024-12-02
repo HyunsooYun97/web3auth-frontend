39c3f245f8d0166aa15b2aa936ca78b59a9f358f
HyunsooYun97 <hyensooyoon@primeblock.co>
2024-12-02 16:17:30 +0900

    test: 로그인 테스트20

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,6 +23,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      beforeEnter: async (to, from, next) => {
+        const web3authStore = useWeb3AuthStore();
+
+        await web3authStore.init();
+        next();
+      },
     },
     {
       path: '/sign-in',
@@ -33,12 +39,6 @@ const router = createRouter({
       path: '/wallet',
       name: 'wallet',
       component: WalletView,
-      beforeEnter: async (to, from, next) => {
-        const web3authStore = useWeb3AuthStore();
-
-        await web3authStore.init();
-        next();
-      },
     },
     ...redirect,
   ],
